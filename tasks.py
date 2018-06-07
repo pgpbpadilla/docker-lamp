5afef3ebc0d9062d62dad75660d14afad56c3dac
Pablo Padilla <pablo.g.padilla.-ng@disney.com>
2018-06-07 15:58:22 -0700

    fix: usr: Downgrade to MySQL 5.
    Suggested fix: https://github.com/laradock/laradock/issues/1350

--- tasks.py
+++ tasks.py
@@ -1,12 +1,21 @@
 from invoke import task
+import os, shutil
 
 @task
 def build(ctx):
     '''Build Docker images defined in `docker-compose.yml`.'''
     ctx.run('docker-compose build')
 
+@task
+def clean(ctx):
+    '''Remove data created by previous run'''
+    print('Clean up old data ...')
+    tmp_data_dir = './db/data'
+    if os.path.exists(tmp_data_dir):
+        shutil.rmtree(tmp_data_dir)
+    print('It worked!')
 
-@task(pre=[build])
+@task(pre=[clean, build])
 def up(ctx):
     '''Run the containers as defined in `docker-compose.yml`.
     '''
